a9a0542a2b827e84125f8223e6b866dac0b7dbda
leehyeonj <billaa@naver.com>
2022-03-11 23:39:28 +0900

    test

--- 위장.js
+++ 위장.js
@@ -1,4 +1,7 @@
 function solution(clothes) {
 	return Object.values(clothes.reduce((obj, t)=> {
+		console.log('hih')
 		obj[t[1]] = obj[t[1]] ? obj[t[1]] + 1 : 1;
 		return obj; } , {})).reduce((a,b)=> a*(b+1), 1)-1; }
+
+		
